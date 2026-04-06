f740af85fe2d50d30438bd312624e879f2fd6dae
Waqar6489 <waqarali6489@gmail.com>
2026-04-06 21:40:02 +0500

    add api file

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+// This is a stub for Week 2 Backend Integration
+export const API_BASE_URL = "http://127.0.0.1:8000";
+
+export const testConnection = () => {
+    console.log("Checking connection to:", API_BASE_URL);
+};
